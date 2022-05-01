69fa576aebc9ec0bddedc3cbe844a714b28eaf29
Зайцева Татьяна <rtvtat@rambler.ru>
2022-05-02 01:20:56 +0300

    Init commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 2965;
+        int amountMili = 20;
+
+        int bonus = price / amountMili;
+        System.out.println("Количество начисленных милей:" + bonus);
+    }
+}
